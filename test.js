fbca01b8260a301871a28371eef70a652e77d694
SGSouthard <sydneyaileensouthard@gmail.com>
2022-03-18 17:11:10 -0700

    working on isPanagram

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 /*
 string.charCodeAt(index) - 96
+1 && 2 && 3 && 4 && 5 && 6 && 7 && 8 && 9 && 10 && 11 && 12 && 13 && 14 && 15 && 16 && 17 && 18 && 19 && 20 && 21 && 22 && 23 && 24 && 25 && 26)
 
 Step 1: Convert string to lower case
 Step 2: Get index/decimal for each letter
@@ -12,16 +13,40 @@ function isPangram(string){
     console.log(string)
     const panString = string.toLowerCase()
 
-    
-    
+    let numArr = []
 
-    return panString
+    for (i = 0; i < panString.length; i++) {
+        let y = panString.charCodeAt(i) - 96
+        if (y < 0 || y > 26) {
+            
+        } else {
+            numArr.push(panString.charCodeAt(i) - 96)
+        } 
+    }
+
+    if (numArr.includes(1 && 2 && 3 && 4 && 5 && 6 && 7 && 8 && 9 && 10 && 11 && 12 && 13 && 14 && 15 && 16 && 17 && 18 && 19 && 20 && 21 && 22 && 23 && 24 && 25 && 26)) {
+        answer = true
+    } else {
+        answer = false
+    }
+
+    // answer = true
+
+    // for (i = 1; i < 26;) {
+    //     if (numArr.includes(i)) {  
+    //         i++
+    //     } else {
+    //         answer = false
+    //     }
+    // }
+    
+    return answer
 }
 
 console.log(isPangram("The quick brown fox jumps over the lazy dog."), "Should be True")
-// console.log("---------------------------------------------------------")
-// console.log(isPangram("This is not a pangram."), "Should be False")
-// console.log("---------------------------------------------------------")
-// console.log(isPangram("She sells seashells down by the seashore."), "Should be False")
-// console.log("---------------------------------------------------------")
-// console.log(isPangram("Two driven jocks help fax my big quiz.​"), "Should be True")
+console.log("==============================================================")
+console.log(isPangram("This is not a pangram."), "Should be False")
+console.log("==============================================================")
+console.log(isPangram("She sells seashells down by the seashore."), "Should be False")
+console.log("==============================================================")
+console.log(isPangram("Two driven jocks help fax my big quiz.​"), "Should be True")
