d44277d366bddeae4c7c8416e3d171b9d465ccae
SGSouthard <sydneyaileensouthard@gmail.com>
2022-03-19 23:54:30 -0700

    working on problem

--- test.js
+++ test.js
@@ -1,50 +1,25 @@
 /*
-string.charCodeAt(index) - 96
-1 && 2 && 3 && 4 && 5 && 6 && 7 && 8 && 9 && 10 && 11 && 12 && 13 && 14 && 15 && 16 && 17 && 18 && 19 && 20 && 21 && 22 && 23 && 24 && 25 && 26
-"a" && "b" && "c" && "d" && "e" && "f" && "g" && "h" && "i" && "j" && "k" && "l" && "m" && "n" && "o" && "p" && "q" && "r" && "s" && "t" && "u" && "v" && "w" && "x" && "y" && "z"
-
-Step 1: Convert string to lower case
-Step 2: Get index/decimal for each letter
-Step 3: check that the string has numbers 1-26
-Step 4: Return as true or false
-
+You get an array of numbers, return the sum of all of the positives ones.
 */
 
-function isPangram(string){
-    string = string.toLowerCase()
-    alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+function positiveSum(arr) {
 
-    return alphabet.every(x => string.includes(x));
+    posArr = arr.filter(checkPositive)
+  
+    function checkPositive(num) {
+        return num > 0;
+    }
 
-    // console.log(string)
-    // const panString = string.toLowerCase()
+    console.log(posArr)
 
-    // let numArr = []
+    if ()
 
-    // for (i = 0; i < panString.length; i++) {
-    //     let y = panString.charCodeAt(i) - 96
-    //     if (y < 0 || y > 26) {
-            
-    //     } else {
-    //         numArr.push(panString.charCodeAt(i) - 96)
-    //     } 
-    // }
+    // let reducer = (accumulator, current) => accumulator + current;
+    // let sum = posArr.reduce(reducer)
 
-    // if (numArr.includes(1 && 2 && 3 && 4 && 5 && 6 && 7 && 8 && 9 && 10 && 11 && 12 && 13 && 14 && 15 && 16 && 17 && 18 && 19 && 20 && 21 && 22 && 23 && 24 && 25 && 26)) {
-    //     answer = true
-    // } else {
-    //     answer = false
-    // }
-
-    // return answer
+    return
 }
 
-console.log(isPangram("The quick brown fox jumps over the lazy dog."), "Should be True")
-console.log("==============================================================")
-// console.log(isPangram("This is not a pangram."), "Should be False")
-// console.log("==============================================================")
-// console.log(isPangram("She sells seashells down by the seashore."), "Should be False")
-// console.log("==============================================================")
-// console.log(isPangram("Two driven jocks help fax my big quiz.​"), "Should be True")
-// console.log("==============================================================")
-console.log(isPangram("abcdefghijklmopqrstuvwxyz​"), "Should be False")
+console.log(positiveSum([1,2,3,4,5]), 15)
+console.log(positiveSum([-1,-2,-3,-4,-5]), 0)
+console.log(positiveSum([1,-2,3,4,5]),13)
