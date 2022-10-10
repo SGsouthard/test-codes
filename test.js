c0f00105e75432a499219ae91faae710b9ab2e3f
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-10-10 16:54:28 -0700

    Update test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+function createPhoneNumber(numbers){
+    
+    console.log(phoneNumber);
+    return phoneNumber;
+}
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
