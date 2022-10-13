c78f46d4aa2864882edc603116dd924d7ab9962d
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-10-13 01:22:30 -0700

    Update test.js

--- test.js
+++ test.js
@@ -2,11 +2,21 @@ function createPhoneNumber(numbers){
     let firstPart = []
     let secondPart = []
     let thirdPart = []
+    let phoneNumber = ""
 
     for (let i = 0; i< numbers.length; i++) {
-        console.log(i)
+        // console.log(i)
+        if (i < 3) {
+            firstPart.push(numbers[i])
+        } else if (i < 6) {
+            secondPart.push(numbers[i])
+        } else if (i < 10) {
+            thirdPart.push(numbers[i])
+        } else {
+            console.log("nah")
+        }        
     }
-
+    
 }
 
 createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
