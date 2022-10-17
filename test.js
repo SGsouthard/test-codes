afd9484db5b9b907aa9785ae1ce6ae0ad77503dd
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-10-17 01:22:12 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,26 +1,17 @@
-function createPhoneNumber(numbers){
-    let firstPart = []
-    let secondPart = []
-    let thirdPart = []
-    let phoneNumber = ""
-
-    for (let i = 0; i< numbers.length; i++) {
-        // console.log(i)
-        if (i < 3) {
-            firstPart.push(numbers[i])
-        } else if (i < 6) {
-            secondPart.push(numbers[i])
-        } else if (i < 10) {
-            thirdPart.push(numbers[i])
-        } else {
-            console.log("nah")
-        }        
+function validatePIN (pin) {
+    //return true or false
+    
+    if (false) {
+        return false
+    } else {
+        return false
     }
-    phoneNumber = "(" + firstPart.join("") + ") " + secondPart.join("") + "-" + thirdPart.join("")
-    console.log("phone number = ", phoneNumber);
-    return phoneNumber;
-}
+  }
 
-createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
-createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+validatePIN("1234")
+validatePIN("123456")
+validatePIN("a123")
+validatePIN("a12345")
+validatePIN("12345")
+validatePIN(a123)
+validatePIN(1234)
